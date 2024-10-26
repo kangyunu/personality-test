d1024906cc2d170072243616b03b75c707975123
yunu.kang <yunu.kang@dnotitia.com>
2024-10-26 14:22:36 +0900

    feat: implement type page

--- src/app/big-five-hexaco/[id]/page.tsx
+++ src/app/big-five-hexaco/[id]/page.tsx
@@ -1,16 +1,45 @@
 import { notFound } from "next/navigation";
 import types from "@/data/big-five-hexaco/types.json";
 
-const BigFiveHexacoResultPage = ({ params }: { params: { id: string } }) => {
+const getType = (id: string) => types.find((type) => type.id === id);
+
+export const generateMetadata = ({ params }: { params: { id: string } }) => {
   const { id } = params;
 
-  const type = types.find((type) => type.id === id);
+  const type = getType(id);
+  if (!type) return undefined;
 
-  if (!type) notFound();
+  return {
+    title: type.name,
+    description: type.description,
+  };
+};
+
+const BigFiveHexacoResultPage = ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+  const type = getType(id);
+
+  if (!type) return notFound();
 
   return (
-    <div>
-      <pre>{JSON.stringify(type, null, 2)}</pre>
+    <div className="max-w-2xl m-auto">
+      <div className="h-96 bg-red-50" />
+
+      <div className="text-2xl text-bold mt-4 pl-4">{type.name}</div>
+      <div className="text-xl text-gray-600 pl-4">({type.english})</div>
+
+      <div className="flex gap-2 justify-start pl-4 mt-2">
+        {type.characters.map((character) => (
+          <div
+            key={character}
+            className="rounded-lg border border-gray-400 px-2 font-semibold text-sm bg-gray-400 text-white"
+          >
+            {character}
+          </div>
+        ))}
+      </div>
+
+      <div className="mt-4 pl-4">{type.description}</div>
     </div>
   );
 };
